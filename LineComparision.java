efa64ab2e30d72c471f107d794f9a1e9971fad3f
shindeanant <shindeanant1997@gmail.com>
2021-09-20 16:02:49 +0530

    Calculate Length

--- LineComparision.java
+++ LineComparision.java
@@ -8,6 +8,19 @@ public class LineComparision {
 
 		System.out.println("Welcome to Line Comparison Computation Program");
 
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter the values:");
+
+		double x1 = scan.nextDouble();
+		double x2 = scan.nextDouble();
+		double y1 = scan.nextDouble();
+		double y2 = scan.nextDouble();
+
+		double len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("length is:" + len1);
+
 	}
 
 }
